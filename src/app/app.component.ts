acd49eee3b7a8b402d570956a8159181b8bdafce
eixon <eixondltf.19@gmail.com>
2023-11-08 22:03:32 -0500

    mi primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ApiService } from './service/api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular_project';
+  constructor(private ApiService:ApiService){
+    this.ApiService.obtener
+  }
+}
